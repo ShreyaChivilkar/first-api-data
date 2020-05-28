c08cea1664d158596fd51a4c4f037e3b763f1725
ShreyaChivilkar <shreyasc1999@gmail.com>
2020-05-28 22:47:16 +0530

    Initial commit

---
+++ src/main/java/com/example/demo/FirstApiDataApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstApiDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstApiDataApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/demo/topic/TopicService.java
@@ -0,0 +1,42 @@
+package com.example.demo.topic;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TopicService {
+	
+	@Autowired
+	private TopicRepository topicRepository;
+	
+	public List<Topic>getAllTopic()
+	{
+		List<Topic>topics = new ArrayList<>();
+		topicRepository.findAll().forEach(topics :: add);
+		return topics;
+	}
+	
+	public Topic getTopic(String id)
+	{
+		return topicRepository.findById(id).orElse(null);
+	}
+
+	public void addTopic(Topic topic) {
+		topicRepository.save(topic);
+		
+	}
+
+	public void updateTopic(String id, Topic topic) {
+		topicRepository.save(topic);
+	}
+
+
+	public void deleteTopic(String id) {
+		topicRepository.deleteById(id);
+		
+	}
+
+}
